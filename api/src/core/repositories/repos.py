45a17d479457eb172461600f72239eb3a493181b
Oleg Kuzmenko <olegkuzmenko2005@gmail.com>
2023-11-08 23:04:36 +0200

    Added models for projects and projects tags. Added repos for them

--- api/src/core/repositories/repos.py
+++ api/src/core/repositories/repos.py
@@ -1,6 +1,15 @@
 from src.auth.models import JWTTokensBlackList
+from src.projects.models import Project, ProjectTag
 from src.core.utils.repository import SQLAlchemyRepository
 
 
 class JWTTokensBlackListRepository(SQLAlchemyRepository):
     model = JWTTokensBlackList
+
+
+class ProjectRepository(SQLAlchemyRepository):
+    model = Project
+
+
+class ProjectTagRepository(SQLAlchemyRepository):
+    model = ProjectTag
--- api/src/main.py
+++ api/src/main.py
@@ -1,16 +1,19 @@
 from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
 # from src.api_key import get_api_key
 from src.auth.api import router as auth_router
+from src.projects.api import router as projects_router
 
 origins = ['http://localhost:3000']
 
-routers = [auth_router]
+routers = [auth_router, projects_router]
 
 app = FastAPI(
     title='Portfolio'
 )
+app.mount('/media', StaticFiles(directory='media'), name='media')
 
 for router in routers:
     app.include_router(router, prefix='/api/v1')
