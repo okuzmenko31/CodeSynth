7b04500f388d738172fc14d37e1739d9d85d5005
Vladyslav Malyshenko <koooilrekt@gmail.com>
2023-11-10 22:37:40 +0200

    Added some new components and also finished main page

--- web/src/components/UI/ProjectTemplate.tsx
+++ web/src/components/UI/ProjectTemplate.tsx
@@ -1,11 +1,21 @@
 import React from "react"
 import "../../styles/Project.css"
 
-const Project = ({name, tags}: { name: string, tags?: any[] }) => {
+import { addClassOnScroll } from "../../utils/add_class_on_scroll";
+
+const Project = ({name, image, tags}: { name: string, image: any, tags?: any[] }) => {
+
+    const handleScrollFunction = addClassOnScroll(['.project-block-wrapper', 'not_scrolled'], Math.floor(window.innerHeight / 4));
+
+    window.addEventListener('scroll', handleScrollFunction);
+    document.addEventListener('DOMContentLoaded', handleScrollFunction);
+
     return(
         <div className="project-container">
-            <div className="project-block">
-                dsdadsadas
+            <div className="project-block-wrapper">
+                <div className="project-block">
+                    <img alt={name} src={image} className="project-image-preview"/>
+                </div>
             </div>
 
             <div className="project-tags-block">
---
+++ web/src/utils/parallax_effect.ts
@@ -0,0 +1,7 @@
+export const initParallaxEffect = (target: string, speed: number, additional?: string): EventListener => {
+    return function () {
+        let scrollPosition = window.scrollY;
+        let parallaxContent = document.querySelector(target) as HTMLElement;
+        parallaxContent.style.transform = `translate(0, -${scrollPosition * speed}px) ${additional}`;
+    };
+};
