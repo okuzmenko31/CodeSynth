615607ce6eee7338a0e40162b08c39f02e5ed071
Oleg Kuzmenko <oleg.kuzmenko@ronis.info>
2023-12-23 20:30:30 +0200

    Backend APP was dockerized

--- api/src/main.py
+++ api/src/main.py
@@ -19,44 +19,38 @@ from .auth.api import router as auth_router
 from .projects.api import router as projects_router
 from .project_requests.api import router as project_requests_router
 
-origins = [
-    'http://localhost:3000'
-]
+origins = ["http://localhost:3000"]
 
-routers = [
-    auth_router,
-    projects_router,
-    project_requests_router
-]
-
-app = FastAPI(
-    title='Portfolio'
-)
-app.mount(
-    f'/{settings.media.media_file_path_start}',
-    StaticFiles(directory=f'{settings.media.media_directory}'),
-    name=f'{settings.media.media_name}'
-)
-
-for router in routers:
-    app.include_router(router, prefix='/api/v1')
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*']
-)
-
-# Adding pagination to the app
-add_pagination(app)
+routers = [auth_router, projects_router, project_requests_router]
 
 
 @asynccontextmanager
-async def startup(app: FastAPI):
+async def lifespan(app: FastAPI):  # noqa
     caching_backend = InMemoryBackend()
     if settings.caching.use_redis:
         redis = aioredis.from_url(settings.caching.redis_url)
         caching_backend = RedisBackend(redis)
     FastAPICache.init(caching_backend, prefix="fastapi-cache")
+    yield
+
+
+app = FastAPI(title="CodeSynth", lifespan=lifespan)
+app.mount(
+    f"/{settings.media.media_file_path_start}",
+    StaticFiles(directory=f"{settings.media.media_directory}"),
+    name=f"{settings.media.media_name}",
+)
+
+for router in routers:
+    app.include_router(router, prefix="/api/v1")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Adding pagination to the app
+add_pagination(app)
