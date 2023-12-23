5adfac226cedfbfc6bc436578a1d0ac568b79f9c
Oleg Kuzmenko <oleg.kuzmenko@ronis.info>
2023-12-24 01:48:06 +0200

    Added README.md on api side and not important changes in debug_helper.py

---
+++ api/README.md
@@ -0,0 +1,1 @@
+# CodeSynth API Details
--- api/src/core/debug_helper.py
+++ api/src/core/debug_helper.py
@@ -2,14 +2,14 @@ import os
 
 from dotenv import load_dotenv
 
-load_dotenv(dotenv_path=".env.main")
+load_dotenv(dotenv_path=".env")
 
 
 class DebugHelper:
     debug = os.getenv("DEBUG") == "True"
-    dotenv_main_path = ".env.level"
+    dotenv_main_path = ".env"
 
     def get_dotenv_path_by_debug(self):
         if self.debug:
-            return self.dotenv_main_path.replace("level", "local")
-        return self.dotenv_main_path.replace("level", "production")
+            return self.dotenv_main_path + ".local"
+        return self.dotenv_main_path + ".production"
