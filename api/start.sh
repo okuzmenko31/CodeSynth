8cca4a165458464597381b2ab73648293f0bec50
Oleg Kuzmenko <oleg.kuzmenko@ronis.info>
2023-12-24 02:13:11 +0200

    Added API Key protection

--- api/start.sh
+++ api/start.sh
@@ -7,7 +7,7 @@ cd /apps/api
 
 alembic upgrade head
 
-if [ "$(grep -i '^DEBUG=True' .env.main)" ]; then
+if [ "$(grep -i '^DEBUG=True' .env)" ]; then
     uvicorn src.main:app --reload --host 0.0.0.0
 else
     gunicorn src.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000 --reload
