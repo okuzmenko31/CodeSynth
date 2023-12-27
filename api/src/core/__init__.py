6a625255a18e96797e44aba85901420ab9249d0b
Oleg Kuzmenko <oleg.kuzmenko@ronis.info>
2023-12-27 23:06:33 +0200

    Added faqs model

--- api/src/core/__init__.py
+++ api/src/core/__init__.py
@@ -1,3 +1,4 @@
 import src.auth.models as auth_models
 import src.projects.models as project_models
 import src.project_requests.models as project_requests_models
+import src.faqs.models as faqs_models
